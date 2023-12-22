a5ad98b4ec84fd094cf18d60eec090f7e2d437be
starninjas <36938375+starninjas@users.noreply.github.com>
2023-12-21 17:50:16 -0700

    Bug fix O.o

--- mods/mobs/api.lua
+++ mods/mobs/api.lua
@@ -3343,10 +3343,10 @@ function mob_class:get_nodes()
 
 	-- nodes in front of mob and front/above
     self.looking_at = node_ok({
-        x = pos.x + dir_x, y = pos.y + y_level + 0.25, z = pos.z + dir_z}) or {name=""}
+        x = pos.x + dir_x, y = pos.y + y_level + 0.25, z = pos.z + dir_z}) or {name="air"}
 
     self.looking_above = node_ok({
-        x = pos.x + dir_x, y = pos.y + y_level + 1.25, z = pos.z + dir_z}) or {name=""}
+        x = pos.x + dir_x, y = pos.y + y_level + 1.25, z = pos.z + dir_z}) or {name="air"}
 
     self.looking_at = self.looking_at.name
     self.looking_above = self.looking_above.name
